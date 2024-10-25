e9f51460aaf1a933ed1fb37d90b38d08037af5fe
defining-art <118932320+defining-art@users.noreply.github.com>
2024-10-25 02:57:38 -0400

    Speechify Testing

---
+++ SpeechifyTests/SpeechifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechifyTests.swift
+//  SpeechifyTests
+//
+//  Created by Oladapo Emmanuel Oladele on 10/24/24.
+//
+
+import Testing
+@testable import Speechify
+
+struct SpeechifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
