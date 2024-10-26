ba06111dd036f4e5698e3d314bbd5d117bd53d59
defining-art <118932320+defining-art@users.noreply.github.com>
2024-10-25 20:08:23 -0400

    Building & Testing UI Layouts Associated With userHomePage

--- Speechify/userHomePage.swift
+++ Speechify/userHomePage.swift
@@ -5,26 +5,22 @@
 //  Created by Oladapo Emmanuel Oladele on 10/24/24.
 //
 
-
 import SwiftUI
 
-struct userHomePageView: View{
-    @State private var isUserName: String
+struct userHomePage: View{
+    @State private var isUserName: String = ""
     @State private var hasUserImage: Bool = false
     @State private var viewSettings: Bool = false
+    
     var body: some View{
         NavigationView{
-            VStack{
+            VStack(){
                 HStack{
-                    NavigationLink(destination: loginPageView()){
-                        Button(action: {
-                        }){
-                            HStack{
-                                Image(systemName:"arrow.left.square.fill") // Find category symbol, the 4 square one
-                            }
+                    HStack{
+                        NavigationLink(destination: loginPageView()){
+                            Image(systemName:"square.grid.2x2.fill").resizable().scaledToFit().frame(width: 25, height: 25)
                         }
-                    }
-                    Spacer()
+                    }.frame(maxWidth: .infinity, alignment: .trailing)
                     Menu{
                         Button(action: {
                         }){
@@ -47,25 +43,20 @@ struct userHomePageView: View{
                                 Text("Sign Out")
                             }
                         }
-                    }{
+                    }label: {
                         HStack{
-                            Text(isUserName)
-                            Image(systemName:"person.fill").resizable()
+                            Text(isUserName).frame(height:25)
+                            Image(systemName:"person.fill").resizable().scaledToFit().frame(width: 25, height: 25)
                         }
-                    }
-                    .buttonStyle(.bordered).cornerRadius(10)
-                    Spacer()
-                }
-                wordCard()
-            }
-            .background{
-                Color(.lightGray).ignoresSafeArea()
+                    }.padding(5).background(Color(UIColor.systemGray5)).clipShape(RoundedRectangle(cornerRadius: 5)).frame(maxWidth: .infinity, alignment: .leading)
+                }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                //wordCard()
             }
         }
     }
 }
 
-struct wordCard: View{
+/*struct wordCard: View{ 
     /*
     struct wordCard<isFront: View, isBack: View>: View{
     let frontCard: () -> isFront
@@ -95,7 +86,7 @@ struct wordCard: View{
                     }
                 }
                 Spacer()
-                cardFace(frontCard:{Text(currentWord)}, backCard:{Text(isPhoneticSpelling)}, isFaceWord: $isFaceWord)
+                cardFace(frontCard:{Text("front"/*currentWord*/)}, backCard:{Text("Back"/*isPhoneticSpelling*/)}/*, isFaceWord: $isFaceWord*/)
                 Spacer()
                 Button(action: {
                     fetchNextWord()
@@ -105,11 +96,8 @@ struct wordCard: View{
                     }
                 }
             }
-        }
-        .rotation3DEffect(.degrees(cardRotation), axis: (x: 0, y: 1, z: 0))
-        .frame(width: 400, height: 200).background(.white)
-        .rotation3DEffect(.degrees(cardContentRotation), axis: (x: 0, y: 1, z: 0))
-        .overlay(
+        }.rotation3DEffect(.degrees(cardRotation), axis: (x: 0, y: 1, z: 0)).frame(width: 400, height: 200).background(.white).rotation3DEffect(.degrees(cardContentRotation), axis: (x: 0, y: 1, z: 0))
+            .overlay(
             Rectangle().stroke(.black, lineWidth: 2)
             VStack{
                 Button(action: {
@@ -171,7 +159,90 @@ struct cardFace<isFront: View, isBack: View>: View{
         }
     }
 }
+*/ //Card UI Loading.....
+
+struct userProfileView: View{
+    var body: some View{
+        VStack{
+            Text("User Profile")
+        }
+    }
+}
+
+struct userFavouritesView: View{ // Add a little section at the bottom that shows the language , theme , phonetic spelling and an example of the word being used gramticaly (AI)
+    var body: some View{
+        VStack{
+            VStack{
+                HStack{
+                    Image(systemName:"arrow.left.square.fill").resizable().scaledToFit().frame(width: 50, height: 50)
+                    Text("Favourites").frame(width: 275, height: 175).background(Color(UIColor.systemGray5)).clipShape(RoundedRectangle(cornerRadius: 10))
+                    Image(systemName:"arrow.right.square.fill").resizable().scaledToFit().frame(width:50, height: 50)
+                }.padding()
+            }.background(Color.gray)
+        }
+        VStack{
+            HStack{
+                Text("Info")
+                // Display information here
+            }
+        }.background(Color.gray)
+    }
+}
+
+struct themeView: View{ // Replace the systemImages & themes names and add some animations and stuff
+    @State private var themeOptions: [String] = ["Random", "Greetings"]
+    var body: some View{
+        ScrollView{
+            VStack{
+                Text("Theme").font(.largeTitle).frame(maxWidth: .infinity, alignment: .center).padding(.top, 10)
+                //ForEach(Array(themeOptions.enumerated()), id: \.element){ index, item in
+                HStack{
+                    VStack{
+                        Image(systemName: "arrow.left").resizable().scaledToFit().frame(width: 175, height: 175)
+                        Text("Theme Name")
+                    }.padding(5).background(Color(UIColor.systemGray5)).clipShape(RoundedRectangle(cornerRadius: 10))
+                    VStack{
+                        Image(systemName: "arrow.right").resizable().scaledToFit().frame(width: 175, height: 175)
+                        Text("Theme Name")
+                    }.padding(5).background(Color(UIColor.systemGray5)).clipShape(RoundedRectangle(cornerRadius: 5))
+                }.padding(10)
+                HStack{
+                    VStack{
+                        Image(systemName: "arrow.up").resizable().scaledToFit().frame(width: 175, height: 175)
+                        Text("Theme Name")
+                    }.padding(5).background(Color(UIColor.systemGray5)).clipShape(RoundedRectangle(cornerRadius: 10))
+                    VStack{
+                        Image(systemName: "arrow.down").resizable().scaledToFit().frame(width: 175, height: 175)
+                        Text("Theme Name")
+                    }.padding(5).background(Color(UIColor.systemGray5)).clipShape(RoundedRectangle(cornerRadius: 5))
+                }.padding(10)
+                HStack{
+                    VStack{
+                        Image(systemName: "arrow.left").resizable().scaledToFit().frame(width: 175, height: 175)
+                        Text("Theme Name")
+                    }.padding(5).background(Color(UIColor.systemGray5)).clipShape(RoundedRectangle(cornerRadius: 10))
+                    VStack{
+                        Image(systemName: "arrow.right").resizable().scaledToFit().frame(width: 175, height: 175)
+                        Text("Theme Name")
+                    }.padding(5).background(Color(UIColor.systemGray5)).clipShape(RoundedRectangle(cornerRadius: 5))
+                }.padding(10)
+                HStack{
+                    VStack{
+                        Image(systemName: "arrow.up").resizable().scaledToFit().frame(width: 175, height: 175)
+                        Text("Theme Name")
+                    }.padding(5).background(Color(UIColor.systemGray5)).clipShape(RoundedRectangle(cornerRadius: 10))
+                    VStack{
+                        Image(systemName: "arrow.down").resizable().scaledToFit().frame(width: 175, height: 175)
+                        Text("Theme Name")
+                    }.padding(5).background(Color(UIColor.systemGray5)).clipShape(RoundedRectangle(cornerRadius: 5))
+                 }.padding(10)
+                    //}
+                }
+            }
+        }
+    }
+
 
 #Preview {
-    userHomePageView()
+    userFavouritesView()
 }
