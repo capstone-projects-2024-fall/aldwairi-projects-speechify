75099715ccd71d3bb5da65b9cfc292f2807f156f
amyollomani1 <93606711+amyollomani1@users.noreply.github.com>
2024-10-30 20:36:33 -0400

    Implemented audio recordings

---
+++ SignInView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInView.swift
+//  Speechify
+//
+//  Created by Amy Ollomani on 10/15/24.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text("Sign in!")
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
---
+++ SpeechifyApp.swift
@@ -0,0 +1,19 @@
+//
+//  SpeechifyApp.swift
+//  Speechify
+//
+//  Created by Amy Ollomani on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+// testing
+
+struct SpeechifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
