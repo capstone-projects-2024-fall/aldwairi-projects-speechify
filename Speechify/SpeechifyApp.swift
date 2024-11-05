790557a1fdb4cd91212bd49a65de7887f9b38463
defining-art <118932320+defining-art@users.noreply.github.com>
2024-11-05 00:42:00 -0500

    Configure Database

--- Speechify/SpeechifyApp.swift
+++ Speechify/SpeechifyApp.swift
@@ -6,11 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool{
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
-struct SpeechifyApp: App {
-    var body: some Scene {
-        WindowGroup {
+struct SpeechifyApp: App{
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene{
+        WindowGroup{
             contentPageView()
         }
     }
